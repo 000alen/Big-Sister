4af6bdd7aefda023a2de2693ba61c020a9fe21f3
Juan Pablo Gelmi <jpgelmi@gmail.com>
2021-05-12 22:43:59 -0400

    In progress: no está listo

--- scripts/limpieza_resultados/clean_raw_resultados_rm_diputados.py
+++ scripts/limpieza_resultados/clean_raw_resultados_rm_diputados.py
@@ -1,6 +1,5 @@
 import csv
 import os
-from pathlib import Path
 
 CANDIDATO = "GIORGIO JACKSON DRAGO"
 
@@ -24,16 +23,33 @@ header = [
     "Votos TER"   
 ]
 
-root = Path("../../databases/")
+a = os.path.dirname("../../databases")
+print(a)
 
-#print(root)
+csv_reader = csv.reader(open("/Users/jp/Documents/Programacion/Python/Big-Sister/databases/IN/servel/Resultados_Mesa_DIPUTADOS.csv"), delimiter = ';')
 
-csv_reader = csv.reader(open("/Users/jp/Documents/Programacion/Python/Big-Sister/databases/Resultados_Mesa_DIPUTADOS.csv"), delimiter = ';')
+with open('/Users/jp/Documents/Programacion/Python/Big-Sister/databases/OUT/servel/elecciones_diputados.csv', 'w', newline='') as outcsv:
+    writer = csv.writer(outcsv)
+    writer.writerow(header)
 
-for i in csv_reader:
-    for b in i:
-        if b == CANDIDATO:
-            print(i)
+    #"Region", #0
+    #"Distrito",#3
+    #"Comuna",#4
+    #"Local"#6
+    #"Nro. Mesa",#7
+    #"Tipo Mesa",#8
+    #"Mesas Fusionadas",#9
+    #"Electores",#10
+    #"Nro. En Voto",#11
+    #"Candidato",#15
+    #"Votos TER"#16
+    
+    for i in csv_reader:
+        for b in i:
+            if b == CANDIDATO:
+                outcsv.writelines(i + " ,")
+
+
 
 
 
