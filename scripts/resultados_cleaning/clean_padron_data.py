f575d3fb2282278296aaf88147850b1056008e70
Alen Rubilar <lclc.alen@gmail.com>
2021-05-14 23:11:42 -0400

    Format scripts and remove unused import

--- scripts/resultados_cleaning/clean_padron_data.py
+++ scripts/resultados_cleaning/clean_padron_data.py
@@ -14,6 +14,7 @@ root = os.path.abspath("../")
 
 # "/Big-Sister/databases/OUT/servel/resultados_historicos_diputados.csv"
 
-db_historico = open(root + "/Big-Sister/databases/OUT/servel/clean_diputados_historico.csv", "w")
+db_historico = open(
+    root + "/Big-Sister/databases/OUT/servel/clean_diputados_historico.csv", "w")
 
 db_historico.close()
